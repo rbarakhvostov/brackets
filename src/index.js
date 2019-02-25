973fd31e405cf1e79276f078bd97944b23bad949
rbarakhvostov <rbarakhvostov@gmail.com>
2019-02-26 00:34:19 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0, l = bracketsConfig.length; i < l; i++) {
+    bracketsConfig[i] = bracketsConfig[i].join(""); // [['(', ')'], ['[', ']']] ==> ['()', '[]'] 
+  };
+  
+  for (;;) {
+    let strForCompare = str;
+
+    for (let i = 0, l = bracketsConfig.length; i < l; i++) {
+      str = str.split(bracketsConfig[i]).join(""); // '[[()]]' ==> '[[]]' ==> '[]' ==> '' 
+    };
+    
+    if (strForCompare === str) {   // if there are no changes in the string, then false
+
+      for (let i = 0, l = bracketsConfig.length; i < l; i++) {
+        bracketsConfig[i] = bracketsConfig[i].split("");  // ['()', '[]'] ==> [['(', ')'], ['[', ']']]
+                                                          // for test.js   
+      };
+      
+      return false;
+    };
+    
+    if (str === "") break;                                        
+  };
+
+  for (let i = 0, l = bracketsConfig.length; i < l; i++) {
+    bracketsConfig[i] = bracketsConfig[i].split("");     // ['()', '[]'] ==> [['(', ')'], ['[', ']']] 
+                                                         //for test.js
+  };
+ 
+  return true;   
 }
